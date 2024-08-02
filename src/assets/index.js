7a247c82032181b82166e244967cb4587dcfc2c9
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-02 19:40:46 +0530

    assets index

---
+++ src/assets/index.js
@@ -0,0 +1,13 @@
+import { babylov } from "./babylov.svg";
+import { garten } from "./garten.svg";
+import { netflix } from "./netflix.svg";
+import { unesco } from "./unesco.svg";
+import { who } from "./who.svg";
+
+export {
+    babylov,
+    garten,
+    netflix,
+    unesco,
+    who
+}
