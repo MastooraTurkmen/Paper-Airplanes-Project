e7fa7649cb6250ac858152cc12ad671c19211df0
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-02 19:40:57 +0530

    companies constants

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,32 +1,62 @@
+import { unesco, who, netflix, garten, babylov } from "../assets";
+
 export const navigation = [
-    {
-      id: "0",
-      title: "Home",
-      url: "#hero",
-    },
-    {
-      id: "1",
-      title: "Who We Are",
-      url: "#who-we-are",
-    },
-    {
-      id: "2",
-      title: "Our Programs",
-      url: "#programs",
-    },
-    {
-      id: "3",
-      title: "Join Us",
-      url: "#join",
-    },
-    {
-      id: "4",
-      title: "Partners",
-      url: "#partners",
-    },
-    {
-        id: "5",
-        title: "Blog",
-        url: "#blog",
-    },
-  ];
+  {
+    id: "0",
+    title: "Home",
+    url: "#hero",
+  },
+  {
+    id: "1",
+    title: "Who We Are",
+    url: "#who-we-are",
+  },
+  {
+    id: "2",
+    title: "Our Programs",
+    url: "#programs",
+  },
+  {
+    id: "3",
+    title: "Join Us",
+    url: "#join",
+  },
+  {
+    id: "4",
+    title: "Partners",
+    url: "#partners",
+  },
+  {
+    id: "5",
+    title: "Blog",
+    url: "#blog",
+  },
+];
+
+export const companies = [
+  {
+    id: "0",
+    title: "UNESCO",
+    src: unesco,
+  },
+  {
+    id: "1",
+    title: "World Health Organization",
+    src: who,
+  },
+  {
+    id: "2",
+    title: "Netflix",
+    src: unesco,
+  },
+  {
+    id: "3",
+    title: "Kinder Garten",
+    src: garten,
+  },
+  {
+    id: "4",
+    title: "Babylov",
+    src: babylov,
+  },
+];
