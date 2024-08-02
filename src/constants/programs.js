58af3a3e3dac03e7c18accd79fbb98bac7bf8ab5
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-02 11:05:35 +0430

    data

---
+++ src/constants/programs.js
@@ -0,0 +1,20 @@
+export const programs = [
+    {
+        id: "1",
+        title: "English Program",
+        text: "Empowering women through technology education and career opportunities.",
+        image: "/images/book.png"
+    },
+    {
+        id: "2",
+        title: "Women in Tech",
+        text: "Empowering women through technology education and career opportunities.",
+        image: "/images/women-in-tech.png"
+    },
+    {
+        id: "3",
+        title: "Development",
+        text: "Fostering growth through skills training and professional development.",
+        image: "/images/development.png"
+    },
+]
