b24c2d281d02c25b87e43e6f146c66028668a123
Ali Sajad Ahmadi <126584124+alisajad001@users.noreply.github.com>
2024-08-02 10:17:00 +0430

    Add Hero.jsx component and add it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
+import Hero from './components/Hero';
+
 const App = () => {
   return (
-    <div className="text-3xl text-center bg-red-400 p-4">Project Start</div>
+    <>
+      <Hero />
+    </>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: { primary: '#1E1D2F', secondary: '#FF8704' },
+    },
   },
-  plugins: [],
+  plugins: [require('daisyui')],
 };
