f8809d87fe99ec856ac735c5647d172e30da8f92
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-03 22:39:34 +0530

    Story component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Companies from "./components/Companies";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import OurPrograms from "./components/Our-Programs";
+import Story from "./components/Story";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <Companies />
+      <Story />
       <OurPrograms />
     </div>
   );
