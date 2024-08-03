0a9938313ab6a0609a25ca2891a7a265be9c9e19
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-03 15:05:32 +0430

    adds more images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,23 @@ import garten from "./companies-logo/garten.svg";
 import netflix from "./companies-logo/netflix.svg";
 import unesco from "./companies-logo/unesco.svg";
 import who from "./companies-logo/who.svg";
+import EnglishStudentChart from "./English-Prgram-Images/English-program-chart-Student.png";
+import EnglishTeacherChart from "./English-Prgram-Images/English-program-chart-Teacher.png";
+import EnglishProgram from "./English-Prgram-Images/English-program.jpeg";
+import Woman from "./English-Prgram-Images/woman.jpeg";
+import WomanInTechStudents from "./English-Prgram-Images/women-in-tech-students.png";
+import WomanInTech from "./English-Prgram-Images/women-in-tech.jpeg";
 
 export {
     baby,
     garten,
     netflix,
     unesco,
-    who
+    who,
+    EnglishProgram,
+    EnglishStudentChart,
+    EnglishTeacherChart,
+    WomanInTechStudents,
+    Woman,
+    WomanInTech
 }
