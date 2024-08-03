e9d487c041b86b90195fd2fc0f44fd73df7db108
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-03 22:38:16 +0530

    current changes

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,11 +3,21 @@ import garten from "./companies-logo/garten.svg";
 import netflix from "./companies-logo/netflix.svg";
 import unesco from "./companies-logo/unesco.svg";
 import who from "./companies-logo/who.svg";
+// Story Section assets
+import manager from "./story-images/manager.jfif";
+import employees from "./story-images/employees.jfif"
+// Rectangles
+import rect1 from "./rectangles/rect1.svg";
+import rect2 from "./rectangles/rect2.svg";
 
 export {
     baby,
     garten,
     netflix,
     unesco,
-    who
+    who,
+    manager,
+    employees,
+    rect1,
+    rect2
 }
