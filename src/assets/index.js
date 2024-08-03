ec34bb55d290103469b6e742acddfb2347dd6799
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-08-03 22:44:21 +0530

    assets index updated

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,12 +3,21 @@ import garten from "./companies-logo/garten.svg";
 import netflix from "./companies-logo/netflix.svg";
 import unesco from "./companies-logo/unesco.svg";
 import who from "./companies-logo/who.svg";
+<<<<<<< HEAD
 // Story Section assets
 import manager from "./story-images/manager.jfif";
 import employees from "./story-images/employees.jfif"
 // Rectangles
 import rect1 from "./rectangles/rect1.svg";
 import rect2 from "./rectangles/rect2.svg";
+=======
+import EnglishStudentChart from "./English-Prgram-Images/English-program-chart-Student.png";
+import EnglishTeacherChart from "./English-Prgram-Images/English-program-chart-Teacher.png";
+import EnglishProgram from "./English-Prgram-Images/English-program.jpeg";
+import Woman from "./English-Prgram-Images/woman.jpeg";
+import WomanInTechStudents from "./English-Prgram-Images/women-in-tech-students.png";
+import WomanInTech from "./English-Prgram-Images/women-in-tech.jpeg";
+>>>>>>> 1d25cb76c94706844b88e452d7c2d9a5e2a4adae
 
 export {
     baby,
@@ -16,8 +25,17 @@ export {
     netflix,
     unesco,
     who,
+<<<<<<< HEAD
     manager,
     employees,
     rect1,
     rect2
+=======
+    EnglishProgram,
+    EnglishStudentChart,
+    EnglishTeacherChart,
+    WomanInTechStudents,
+    Woman,
+    WomanInTech
+>>>>>>> 1d25cb76c94706844b88e452d7c2d9a5e2a4adae
 }
