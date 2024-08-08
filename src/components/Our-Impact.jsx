fa6d8fad444123b7cda152d225ddd6a214067289
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-08 15:02:47 +0430

    fixed some bugs and did responsive

--- src/components/Our-Impact.jsx
+++ src/components/Our-Impact.jsx
@@ -2,14 +2,15 @@
 const Programs = () => {
   return (
         <div className="text-white md:text-center p-5 text-start overflow-hidden pb-10 flex flex-col items-center mt-20" id="who-we-are">
-          <div className="bg-[#E87900] w-16 h-16 
+          <div className="bg-[#E87900] w-12 h-12 
                 rounded-full mr-auto ml-[-48px] md:mb-[-120px] mb-[-90px]
                 md:w-20 md:h-20 md:ml-[-60px]"></div>
             <div className="pt-20 flex flex-col items-center p-5">
-                <h1 className="text-[#E87900] text-sm md:mr-0 ml-5 lg:mb-5 lg:text-[24px] md:text-lg mr-auto font-semibold">
+                <h1 className="text-[#E87900] text-sm md:mr-0 
+                md:ml-5 lg:mb-5 lg:text-[24px] md:text-lg mr-auto font-semibold">
                   Our Impact
                 </h1>
-            <h1 className="text-3xl font-bold pb-5 md:m-3 md:text-5xl">The Women In Tech (WiT) program</h1>
+                <h1 className="text-3xl font-bold pb-5 md:m-3 md:text-5xl">The Women In Tech (WiT) program</h1>
               <p className="text-xs md:mx-10 lg:text-lg lg:mx-28 md:text-base text-[#F5F5F5]">
                   DMany beneficiaries have successfully completed high school,
                   earned bachelor's or master's degrees, but their main challenge
@@ -18,7 +19,11 @@ const Programs = () => {
                   journey with us at the appropriate level (A1, A2, B1, or B2)
                   determined by a comprehensive placement test, which enables them
                   to progress steadily towards their academic and professional goals.
-                </p>
+              </p>
+              <button className="border-none font-semibold md:mr-0 mr-auto rounded-full text-xs 
+              md:text-[18px] mt-5 px-5 py-2 bg-[#E87900] md:px-7 md:py-4">
+              Learn More
+            </button>
             </div>
         </div>
   )
