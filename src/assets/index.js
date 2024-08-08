1b4b91596244523d49a2783451d64a7fa8a08ef0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-08 09:58:00 +0430

    export and import second program images

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,9 +3,11 @@ import garten from "./companies-logo/garten.svg";
 import netflix from "./companies-logo/netflix.svg";
 import unesco from "./companies-logo/unesco.svg";
 import who from "./companies-logo/who.svg";
+
 // Story Section assets
 import manager from "./story-images/manager.jfif";
 import employees from "./story-images/employees.jfif"
+
 // Rectangles
 import rect1 from "./rectangles/rect1.svg";
 import rect2 from "./rectangles/rect2.svg";
@@ -15,11 +17,17 @@ import EnglishProgram from "./English-Prgram-Images/English-program.jpeg";
 import Woman from "./English-Prgram-Images/woman.jpeg";
 import WomanInTechStudents from "./English-Prgram-Images/women-in-tech-students.png";
 import WomanInTech from "./English-Prgram-Images/women-in-tech.jpeg";
+import FemaleStudents from './Programs2/Female-Students.png'
+import GenderDistribution from './Programs2/Gender-distribution.png'
+import HandsTogether from './Programs2/Hands-together.png'
+import StudentsEducation from './Programs2/students-education.png'
+import StudentsUsingComputer from './Programs2/Students-using-computers.png'
+
 // Social Media Icons
 import facebook from "./social-media/facebook.svg";
 import instagram from "./social-media/instagram.svg";
 import linkedin from "./social-media/linkedin.svg";
-import  youtube from "./social-media/youtube.svg";
+import youtube from "./social-media/youtube.svg";
 
 export {
     baby,
@@ -37,6 +45,11 @@ export {
     WomanInTechStudents,
     Woman,
     WomanInTech,
+    FemaleStudents,
+    HandsTogether,
+    GenderDistribution,
+    StudentsEducation,
+    StudentsUsingComputer,
     facebook,
     instagram,
     linkedin,
