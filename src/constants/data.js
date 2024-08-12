50064bce3bc7257e3955b3b2d9515fadd5e2cfdf
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-12 21:09:34 +0430

    adds person data

---
+++ src/constants/data.js
@@ -0,0 +1,22 @@
+export const people = [
+    {
+        id: 1,
+        image: "https://www.shutterstock.com/image-photo/happy-millennial-african-american-man-600nw-1660490494.jpg",
+        text: 'Hamza Saddour, a dedicated English program student at Paper Airplanes, recently met his tutor, Evgenia Dimitriadi, in person after two years of virtual sessions. Evgenia is support helped Hamza secure a full scholarship at Imperial College London, leading him to nominate her for Tutor of the Month/ Year, highlighting the profound impact of their mentorship.'
+    },
+    {
+        id: 2,
+        image: "https://media.istockphoto.com/id/1410538853/photo/young-man-in-the-public-park.webp?b=1&s=170667a&w=0&k=20&c=pGdjFVdK-_BhTa6PMy5VNcXdbxVNngzg-OPzMfJKrG8=",
+        text: 'Hala Jumma Gul, a dedicated English program student at Paper Airplanes, she Empowers women through technology education and career opportunities.'
+    },
+    {
+        id: 3,
+        image: "https://img.freepik.com/premium-photo/portrait-successful-business-woman-with-her-arms-crossed_1284370-73.jpg",
+        text: 'Their dedication and expertise truly made a difference in my learning journey. Hamza was patient, supportive, and always went the extra mile to ensure I understood the concepts thoroughly. Evgenia is support helped Hamza secure a full scholarship at Imperial College London leading him to nominate her for Tutor of the Month/ Year.'
+    },
+    {
+        id: 4,
+        image: "https://img.freepik.com/premium-photo/woman-black-suit-stands-against-grey-background_495907-45.jpg",
+        text: "Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad miitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad miitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+]
