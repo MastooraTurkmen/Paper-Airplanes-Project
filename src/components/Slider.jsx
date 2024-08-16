e359fc46621d5f19ce4bc7e906b31ca59e5322a0
MastooraTurkmen <janmastoorajan@gmail.com>
2024-08-16 21:45:02 +0430

    changed background

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -18,8 +18,8 @@ const Slider = () => {
                         return (
                         <div key={slideIndex} 
                             onClick={()=> getSlide(slideIndex)}
-                                className={`mt-5 transition-all ease-in-out duration-500 cursor-pointer rounded-full bg-orange-400
-                                ${currentIndex === slideIndex ? 'h-[13px] w-[13px] md:h-4 md:w-4': 'h-2 w-2 md:h-[10px] md:w-[10px]'}`}></div>
+                                className={`mt-5 transition-all ease-in-out duration-500 cursor-pointer rounded-full 
+                                ${currentIndex === slideIndex ? 'h-[13px] w-[13px] bg-orange-400 md:h-4 md:w-4': 'bg-white h-2 w-2 md:h-[10px] md:w-[10px]'}`}></div>
                         )
                     })}
                      </div>
