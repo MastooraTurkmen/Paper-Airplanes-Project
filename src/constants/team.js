432ae8d779c9d94eec1eea583e96ce3a1790f5b5
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-18 21:42:19 +0430

    team items

---
+++ src/constants/team.js
@@ -0,0 +1,28 @@
+import { student1, student2, student3 } from "../assets/index";
+
+export const team = [
+    {
+        id: 1,
+        image: student1,
+        name: "Mastoora Turkmen",
+        position: "Software Engineer"
+    },
+    {
+        id: 2,
+        image: student3,
+        name: "Ali Sajad",
+        position: "Software Engineer"
+    },
+    {
+        id: 3,
+        image: student2,
+        name: "Laiba Farooq",
+        position: "UI/UX Designer"
+    },
+    {
+        id: 4,
+        image: student3,
+        name: "M Thoufiq Izhar",
+        position: "Software Engineer"
+    },
+];
