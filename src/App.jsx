d4770ad7284b71e10c977c6ea1ae132a719dd88e
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-18 21:41:46 +0430

    adds team and make all components in one file

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import Companies from './components/Companies';
-import Hero from './components/Hero';
-import Navbar from './components/Navbar';
-import OurPrograms from './components/Our-Programs';
-import OurImpact from './components/Our-Impact';
-import Story from './components/Story';
-import Programs from './components/Programs';
-import Testimonials from './components/Testimonials';
-import Footer from './components/Footer';
-import JoinUs from './components/JoinUs';
-import Programs2 from './components/Programs2';
-import Students from './components/Students';
+import {
+  Companies,
+    Hero,
+    Navbar,
+    OurImpact,
+    OurPrograms,
+    Story,
+    Students,
+    Programs,
+    Programs2,
+    JoinUs,
+    Footer,
+  Testimonials,
+  Team
+} from "./components";
 
 const App = () => {
   return (
@@ -24,6 +27,7 @@ const App = () => {
       <OurImpact />
       <Students />
       <Testimonials />
+      <Team />
       <JoinUs />
       <Footer />
     </div>
