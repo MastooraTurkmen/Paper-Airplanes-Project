96ff6f5ab037eccb2f037e7fceb0b8e1c9e3cb70
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-18 21:41:55 +0430

    make all components in one file

---
+++ src/components/index.js
@@ -0,0 +1,29 @@
+import Companies from './Companies';
+import Hero from './Hero';
+import Navbar from './Navbar';
+import OurPrograms from './Our-Programs';
+import OurImpact from './Our-Impact';
+import Story from './Story';
+import Programs from './Programs';
+import Testimonials from './Testimonials';
+import Footer from './Footer';
+import JoinUs from './JoinUs';
+import Programs2 from './Programs2';
+import Students from './Students';
+import Team from './Team';
+
+export {
+    Companies,
+    Hero,
+    Navbar,
+    OurImpact,
+    OurPrograms,
+    Story,
+    Students,
+    Programs,
+    Programs2,
+    JoinUs,
+    Team,
+    Footer,
+    Testimonials,
+}
