e1474aa3fbe1558f8396a9a73de825004b507798
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-21 18:03:39 +0430

    Our programs page

---
+++ src/pages/OurPrograms.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const OurPrograms = () => {
+  return (
+     <div className="text-center items-center grid h-[100vh]">
+      <h1>Our Programs Page</h1>
+    </div>
+  )
+}
+
+export default OurPrograms
