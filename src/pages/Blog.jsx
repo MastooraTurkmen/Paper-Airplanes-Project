ec52d18e4b619ae6d9eed38de96a8a4e5409ab3f
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-21 18:02:56 +0430

    Blog page

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blog = () => {
+  return (
+     <div className="text-center items-center grid h-[100vh]">
+      <h1>Blog Page</h1>
+    </div>
+  )
+}
+
+export default Blog
