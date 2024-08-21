f9fb41eafeece8c98721b7bb25a8e9381e2f441a
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-21 18:00:42 +0430

    seperated home section into two files

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,37 @@
+import {
+    Companies,
+    OurImpact,
+    OurPrograms,
+    Story,
+    Students,
+    Programs,
+    Programs2,
+    JoinUs,
+    Team,
+    Footer,
+    Testimonials,
+    Hero,
+} from './index'
+
+// HeroSection Component
+function HeroSection() {
+
+  return (
+    <>
+      <Hero />
+      <Companies />
+      <Story />
+      <OurPrograms />
+      <Programs />
+      <Programs2 />
+      <OurImpact />
+      <Students />
+      <Testimonials />
+      <Team />
+      <JoinUs />
+      <Footer />
+    </>
+  );
+}
+
+export default HeroSection;
