e52176bd34b9c74b5a080f8ca9372f74af950bbe
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-21 18:03:28 +0430

    Join Us page

---
+++ src/pages/JoinUs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const JoinUs = () => {
+  return (
+     <div className="text-center items-center grid h-[100vh]">
+      <h1>Join Us Page</h1>
+    </div>
+  )
+}
+
+export default JoinUs
