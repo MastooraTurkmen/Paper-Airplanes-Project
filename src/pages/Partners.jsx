836b66cbf16e5b3ab94a0cca42536384627656fe
Mastoora Turkmen <janmastoorajan@gmail.com>
2024-08-21 18:03:50 +0430

    parteners page

---
+++ src/pages/Partners.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Partners = () => {
+  return (
+     <div className="text-center items-center grid h-[100vh]">
+      <h1>Partners Page</h1>
+    </div>
+  )
+}
+
+export default Partners
