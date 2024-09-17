b4553970c9db595844f6f009ed6add02f400a92d
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-17 22:23:12 +0530

    Mission: Students Animation Completed

--- src/components/Students.jsx
+++ src/components/Students.jsx
@@ -1,21 +1,30 @@
-import React from "react";
 import quoteIcon from "/icons/quote.svg";
 import { students } from "../constants/index";
 import { useState } from "react";
 import chevronLeft from "/icons/chevron-left.svg";
 import chevronRight from "/icons/chevron-right.svg";
+import { motion } from "framer-motion";
 
 const SectionHeading = ({ title, subtitle }) => {
   return (
-    <div className=" flex flex-col items-center p-5">
-      <h1 className="text-2xl font-bold pb-5 md:text-4xl">{title}</h1>
+    <motion.div
+      whileInView={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 200 }}
+      transition={{ duration: 1.5 }}
+      className=" flex flex-col items-center p-5"
+    >
+      <h1 className="text-2xl font-bold pb-5 md:text-4xl"
+      >
+        {title}
+      </h1>
 
       {subtitle && (
-        <p className="text-xs w-72 md:text-base text-[#F5F5F5] md:w-[500px]">
+        <p className="text-xs w-72 md:text-base text-[#F5F5F5] md:w-[500px]"
+        >
           {subtitle}
         </p>
       )}
-    </div>
+    </motion.div>
   );
 };
 
@@ -41,15 +50,21 @@ const Students = () => {
     <section className="text-white overflow-hidden pb-10 flex flex-col items-center mt-32">
       <SectionHeading title="What Our Students Say" />
 
-      <div className="flex gap-3 mt-10 flex-col lg:gap-10 md:flex-row md:mx-12 md:text-center md:items-center">
-        <div className="mx-10 relative">
+      <motion.div whileInView={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 200 }}
+      transition={{ duration: 1.5 }} className="flex gap-3 mt-10 flex-col lg:gap-10 md:flex-row md:mx-12 md:text-center md:items-center">
+        <motion.div whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: -100 }}
+          transition={{ duration: 1 }} className="mx-10 relative">
           <img
             className="rounded-xl md:w-[500px] border"
             src={activeStudents.img}
             alt="A learning student"
           />
-        </div>
-        <div className="mx-10 md:w-[500px] text-start">
+        </motion.div>
+        <motion.div whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }} className="mx-10 md:w-[500px] text-start">
           <img src={quoteIcon} alt="Quote Icon" className="w-14 h-14 mb-5" />
           <p className="text-base sm:text-lg md:text-xl lg:h-36 mt-10">
             {activeStudents.quote}
@@ -86,8 +101,8 @@ const Students = () => {
               </button>
             </div>
           </div>
-        </div>
-      </div>
+        </motion.div>
+      </motion.div>
     </section>
   );
 };
