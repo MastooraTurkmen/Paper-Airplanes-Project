342caca2483b2498bd4387da7ee346e2f91dceed
Thoufiq-Izhar-23 <thoufiqizhar022@gmail.com>
2024-09-18 16:42:34 +0530

    Mission: Testimonials Animation Completed

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,18 +1,31 @@
-import quoteIcon from '/icons/quote.svg';
-import { testimonials } from '../constants/index';
-import { useState } from 'react';
-import chevronLeft from '/icons/chevron-left.svg';
-import chevronRight from '/icons/chevron-right.svg';
+import quoteIcon from "/icons/quote.svg";
+import { testimonials } from "../constants/index";
+import { useState } from "react";
+import chevronLeft from "/icons/chevron-left.svg";
+import chevronRight from "/icons/chevron-right.svg";
+import { motion } from "framer-motion";
 
 const SectionHeading = ({ title, subtitle }) => {
   return (
     <div className=" flex flex-col items-center p-5">
-      <h1 className="text-2xl font-bold pb-5 md:text-4xl">{title}</h1>
+      <motion.h1
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 0, y: 100 }}
+        transition={{ duration: 1 }}
+        className="text-2xl font-bold pb-5 md:text-4xl"
+      >
+        {title}
+      </motion.h1>
 
       {subtitle && (
-        <p className="text-xs w-72 md:text-base text-[#F5F5F5] md:w-[500px]">
+        <motion.p
+          whileInView={{ opacity: 1, x: 0 }}
+          initial={{ opacity: 0, x: 100 }}
+          transition={{ duration: 1 }}
+          className="text-xs w-72 md:text-base text-[#F5F5F5] md:w-[500px]"
+        >
           {subtitle}
-        </p>
+        </motion.p>
       )}
     </div>
   );
@@ -37,23 +50,44 @@ const Testimonials = () => {
   const activeTestimonial = testimonials[activeIndex];
 
   return (
-    <section className="text-white overflow-hidden pb-10 flex flex-col items-center mt-32">
+    <motion.section
+      whileInView={{ opacity: 1, y: 0 }}
+      initial={{ opacity: 0, y: 200 }}
+      transition={{ duration: 1.5 }}
+      className="text-white overflow-hidden pb-10 flex flex-col items-center mt-32"
+    >
       <SectionHeading title="What Our Tutors Say" />
 
       <div className="flex gap-3 mt-10 flex-col lg:gap-10 md:flex-row md:mx-12 md:text-center md:items-center">
         <div className="mx-10 md:w-[500px] text-start">
-          <img src={quoteIcon} alt="Quote Icon" className="w-14 h-14 mb-5" />
-          <p className="text-base sm:text-lg md:text-xl lg:h-36 mt-10">
+          <motion.img
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1 }}
+            src={quoteIcon}
+            alt="Quote Icon"
+            className="w-14 h-14 mb-5"
+          />
+          <motion.p
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: -100 }}
+            transition={{ duration: 1 }}
+            className="text-base sm:text-lg md:text-xl lg:h-36 mt-10"
+          >
             {activeTestimonial.quote}
-          </p>
+          </motion.p>
 
           <div className="flex items-center justify-between my-12">
-            <div>
+            <motion.div
+              whileInView={{ opacity: 1, x: 0 }}
+              initial={{ opacity: 0, x: -100 }}
+              transition={{ duration: 1.3 }}
+            >
               <h2 className="text-base font-bold text-secondary md:text-lg">
                 {activeTestimonial.name}
               </h2>
               <p className="text-xs md:text-sm">{activeTestimonial.role}</p>
-            </div>
+            </motion.div>
 
             <div className="flex items-center">
               <button
@@ -81,14 +115,17 @@ const Testimonials = () => {
         </div>
 
         <div className="mx-10 relative">
-          <img
+          <motion.img
+            whileInView={{ opacity: 1, x: 0 }}
+            initial={{ opacity: 0, x: 100 }}
+            transition={{ duration: 1 }}
             className="rounded-xl md:w-[500px] border"
             src={activeTestimonial.img}
             alt="A Teacher is teaching her students"
           />
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
